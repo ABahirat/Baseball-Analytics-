11f078b2f0bd13b4d6b3ed6174db04c44dfa2160
Robert Ballard <robert.ballard@colorado.edu>
2018-04-26 12:12:43 -0600

    implement frontend logic and name selection

--- frontend.py
+++ frontend.py
@@ -1,11 +1,36 @@
 import tkinter as tk
 from tkinter.ttk import *
+import csv
+
+possibleTeams = []
+teamBatters = {}
+teamPitchers = {}
+
+with open('teams.csv') as csvfile: #instantiate teams
+    reader = csv.DictReader(csvfile)
+    for line in reader: #read the csv
+        value = line["name"]
+        possibleTeams.append(value) #append each team to list of teams
+        teamBatters[value] = [] #and add an empty array to track batters and pitchers for each team
+        teamPitchers[value] = []
+
+with open('batting.csv') as csvfile: #instantiate batters
+    reader = csv.DictReader(csvfile) #reads in the csv where each line is a dictionary based on headers
+    for line in reader:
+        teamBatters[line["team"]].append(line["name"])
+
+with open('pitching.csv') as csvfile: #instantiate pitchers
+    reader = csv.DictReader(csvfile) #reads in the csv where each line is a dictionary based on headers
+    for line in reader:
+        teamPitchers[line["team"]].append(line["name"])
 
 window = tk.Tk()
 window.title("SWP Stat Generator")
 
 homeBatterCombos = []
+homeBatterValues = []
 awayBatterCombos = []
+awayBatterValues = []
 
 #labels
 homeTeamLabel = tk.Label(window,text="Home Team: ")
@@ -21,82 +46,116 @@ awayBatterLabel = tk.Label(window,text="Batters: ")
 awayBatterLabel.grid(column=2,row=3)
 
 homePitcherLabel = tk.Label(window,text="Pitcher: ")
-homePitcherLabel.grid(column=0,row=12)
+homePitcherLabel.grid(column=0,row=13)
 
 awayPitcherLabel = tk.Label(window,text="Pitcher: ")
-awayPitcherLabel.grid(column=2,row=12)
+awayPitcherLabel.grid(column=2,row=13)
 
 vertSpacerTopLabel = tk.Label(window,text=" ")
 vertSpacerTopLabel.grid(column=0,row=2)
 
 vertSpacerMidLabel = tk.Label(window,text=" ")
-vertSpacerMidLabel.grid(column=0,row=13)
+vertSpacerMidLabel.grid(column=0,row=14)
 
 vertSpacerBotLabel = tk.Label(window,text=" ")
-vertSpacerBotLabel.grid(column=0,row=15)
+vertSpacerBotLabel.grid(column=0,row=16)
 
 horiSpacerLabel = tk.Label(window,text="     ")
 horiSpacerLabel.grid(column=4,row=0)
 
 #combobox logic
 def selectHomeTeam(eventObject):
-    #determine list of valid players in string format
-    #for box in homeBatterCombos:
-    #   box['values'] = (###list of valid batters for this team###)
-    #homePitcherCombo['values'] = (###list of valid pitchers for this team###)
+    i = 0
+    for box in homeBatterCombos:
+        box['values'] = teamBatters[homeTeam.get()]
+        box.current(i)
+        i += 1
+    homePitcherCombo['values'] = teamPitchers[homeTeam.get()]
+    homePitcherCombo.current(0)
 
 def selectAwayTeam(eventObject):
-    #determine valid players in string array format
-    #for box in awayBatterCombos:
-    #   box['values'] = (###list of valid batters for this team###)
-    #awayPitcherCombo['values'] = (###list of valid pitchers for this team###)
+    i = 0
+    for box in awayBatterCombos:
+        box['values'] = teamBatters[awayTeam.get()]
+        box.current(i)
+        i += 1
+    awayPitcherCombo['values'] = teamPitchers[awayTeam.get()]
+    awayPitcherCombo.current(0)
 
 #comboboxes
-homeTeamCombo = Combobox(window)
-#homeTeamCombo['values'] = #array of all possible teams
-homeTeamCombo['values'] = ("Colorado Rockies","Just Ameya")
-homeTeamCombo.current(1)
+homeTeam = tk.StringVar()
+homeTeamCombo = Combobox(window, textvariable=homeTeam)
+homeTeamCombo['values'] = possibleTeams
+homeTeamCombo.current(0)
 homeTeamCombo.grid(column=1,row=1)
 homeTeamCombo.bind("<<ComboboxSelected>>", selectHomeTeam)
 
-awayTeamCombo = Combobox(window)
-#awayTeamCombo['values'] = #array of all possible teams
-awayTeamCombo['values'] = ("Colorado Rockies","Just Ameya")
-awayTeamCombo.current(0)
+awayTeam = tk.StringVar()
+awayTeamCombo = Combobox(window, textvariable=awayTeam)
+awayTeamCombo['values'] = possibleTeams
+awayTeamCombo.current(1)
 awayTeamCombo.grid(column=3,row=1)
-homeTeamCombo.bind("<<ComboboxSelected>>", selectAwayTeam)
+awayTeamCombo.bind("<<ComboboxSelected>>", selectAwayTeam)
 
-for i in range(0,8):
-    homeBatterCombos.append(Combobox(window))
+for i in range(0,9):
+    homeBatterValues.append(tk.StringVar())
+    homeBatterCombos.append(Combobox(window, textvariable=homeBatterValues[i]))
     homeBatterCombos[i]['values'] = (".")
     homeBatterCombos[i].current(0)
     homeBatterCombos[i].grid(column=1,row=3+i)
 
-for i in range(0,7):
-    awayBatterCombos.append(Combobox(window))
+for i in range(0,8):
+    awayBatterValues.append(tk.StringVar())
+    awayBatterCombos.append(Combobox(window, textvariable=awayBatterValues[i]))
     awayBatterCombos[i]['values'] = (".")
     awayBatterCombos[i].current(0)
     awayBatterCombos[i].grid(column=3,row=3+i)
 
-homePitcherCombo = Combobox(window)
+homePitcher = tk.StringVar()
+homePitcherCombo = Combobox(window, textvariable=homePitcher)
 homePitcherCombo['values'] = (".")
 homePitcherCombo.current(0)
-homePitcherCombo.grid(column=1,row=12)
+homePitcherCombo.grid(column=1,row=13)
 
-awayPitcherCombo = Combobox(window)
+awayPitcher = tk.StringVar()
+awayPitcherCombo = Combobox(window, textvariable=awayPitcher)
 awayPitcherCombo['values'] = (".")
 awayPitcherCombo.current(0)
-awayPitcherCombo.grid(column=3,row=12)
+awayPitcherCombo.grid(column=3,row=13)
+
+selectHomeTeam(homeTeamCombo)
+selectAwayTeam(awayTeamCombo)
 
 #buttons
-def doCalculation():
+def doCalculate():
+    print("=== HOME TEAM ===")
+    simHomeBatters = ""
+    homeBatters = []
+    for val in homeBatterValues:
+        simHomeBatters += val.get() + ", "
+        homeBatters += val.get()
+    simHomeBatters = simHomeBatters[:-2]
+    print("Batters: " + simHomeBatters)
+    print("Pitcher: " + homePitcher.get())
+    print("=== AWAY TEAM ===")
+    simAwayBatters = ""
+    awayBatters = []
+    for val in awayBatterValues:
+        simAwayBatters += val.get() + ", "
+        awayBatters += val.get()
+    simAwayBatters = simAwayBatters[:-2]
+    print("Batters: " + simAwayBatters)
+    print("Pitcher: " + awayPitcher.get())
     #do the main logic and start the backend
 
-startButton = tk.Button(window,text="Calculate",command=doCalculation)
-startButton.grid(column=1,row=14)
+def doExit():
+    quit()
 
-exitButton = tk.Button(window,text="Exit")
-exitButton.grid(column=3,row=14)
+startButton = tk.Button(window,text="Calculate",command=doCalculate)
+startButton.grid(column=1,row=15)
+
+exitButton = tk.Button(window,text="Exit",command=doExit)
+exitButton.grid(column=3,row=15)
 
 #combobox logic
 
