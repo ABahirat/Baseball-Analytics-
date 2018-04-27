dfac6a814bbc05399bec4bab557cfa881301775d
Robert Ballard <robert.ballard@colorado.edu>
2018-04-26 18:09:40 -0600

    add sql script

--- 2016data.sql
+++ 2016data.sql
@@ -1,5 +1,7 @@
 use lahman2016;
-select distinct concat(m.nameFirst, " ", m.nameLast) as name, t.name as team, (b.H-b.2B-b.3B-b.HR)/(b.H) as 1Bprob, (b.2B)/(b.H) as 2Bprob, (b.3B)/(b.H) as 3Bprob, (b.HR)/(b.H) as HRprob, (b.H)/(b.AB) as BA from batting as b
+select distinct concat(m.nameFirst, " ", m.nameLast) as name, t.name as team, (b.H-b.2B-b.3B-b.HR)/(b.H) as 1Bprob, (b.2B)/(b.H) as 2Bprob, (b.3B)/(b.H) as 3Bprob, (b.HR)/(b.H) as HRprob, (b.H)/(b.AB) as BA,
+(b.H-b.2B-b.3B-b.HR)/(b.H) + (b.2B)/(b.H) + (b.3B)/(b.H) + (b.HR)/(b.H) as totalprob
+from batting as b
 inner join teams as t on t.teamID = b.teamID and t.yearID = b.yearID
 inner join master as m on m.playerID = b.playerID
 where b.yearID = 2016 and b.AB > 30;
